c7104ccea2154569b0973925cb5c1466c8e37bd6
Stephane Vieira <stephane.vieira75@outlook.fr>
2020-10-11 17:13:01 +0200

    Add redirect in App router and create Dashboard scene

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 import './App.css';
 
 import Login from './scenes/Login/Login';
+import Dashboard from './scenes/Dashboard/Dashboard';
 
-import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 
 function App() {
   return (
@@ -11,7 +12,21 @@ function App() {
       <Router>
 
         <Switch>
-          <Route path={"/"} exact component={Login}/>
+          <Route //Login
+            path={"/"}
+            exact
+            component={Login}
+          />
+          <Route // Dashboard
+            path={"/dashboard/"}
+            exact
+            component={Dashboard}
+          />
+          <Route // Redirect
+            path={"*"}
+            exact
+            render={() => <Redirect to="/" />}
+          />
         </Switch>
       </Router>
     </div>
---
+++ src/scenes/Dashboard/Dashboard.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Dashboard() {
+
+    return (
+        <div className="dashboard-container">
+           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae nostrum voluptatem quo deserunt hic error nulla numquam esse aliquid aut voluptate voluptatibus dicta facilis, fugiat quas deleniti eaque dolore delectus?
+        </div>
+    );
+}
+
+export default Dashboard;
